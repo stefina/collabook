bde634ad9459600d8684b5ae4d54d6ecdaf9d772
stefanie.taepke <stefanie.taepke@unic.com>
2015-09-28 23:39:45 +0200

    local-to-remote database sync

--- app/config/Pouch.js
+++ app/config/Pouch.js
@@ -1,6 +1,30 @@
-var PouchDB = require('pouchdb'),
-    db = new PouchDB('http://localhost:5984/collabook', {
-      db : require('pouchdb-find')
-    });
+var BookActions = require('./../scripts/actions/BookActions');
 
-module.exports = db;
+var PouchDB = require('pouchdb'),
+    remoteDB = new PouchDB('http://localhost:5984/collabook', {
+      remoteDB : require('pouchdb-find')
+    }),
+    localDB = new PouchDB('collabook');
+
+localDB.sync(remoteDB, {
+  live: true,
+  retry: true
+}).on('complete', function () {
+  console.log("yay, we're in sync!");
+}).on('change', function (change) {
+  console.log("yo, something changed!");
+  if(change && change.direction && change.direction === "push"){
+    BookActions.updateBooks(change.change.docs[0]);  
+  }
+}).on('paused', function (info) {
+  console.log("replication was paused, usually because of a lost connection");
+}).on('active', function (info) {
+  console.log("replication was resumed");
+}).on('error', function (err) {
+  console.log("boo, we hit an error!");
+}).catch(function(err) {
+  console.log(err);
+});
+
+// module.exports = localDB;
+module.exports = localDB;
